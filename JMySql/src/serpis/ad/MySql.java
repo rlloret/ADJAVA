b571e2ff1f7b3d5981fae51d48def8e8bee3bf32
rlloret <robertolloretbelmonte@gmail.com>
2014-12-15 10:23:39 +0100

    Añadido JUnit

--- JMySql/src/serpis/ad/MySql.java
+++ JMySql/src/serpis/ad/MySql.java
@@ -16,46 +16,7 @@ public class MySql {
 
 		//Class.forName("com.mysql.jdbc.Driver");//Necesario en driver 3 o anterior
 		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba?user=root&password=sistemas");
-
-		
-
-        int opcion=0;
-		do{
-			System.out.println("*****************");
-			System.out.println("Elige una opción:");
-			System.out.println("*****************");
-			System.out.println("1--Borrar");
-			System.out.println("2--Insertar");
-			System.out.println("3--Listar");
-			System.out.println("4--Salir");
-			opcion = scanner.nextInt();
-			
-			
-			switch(opcion){
-		    case 1:
-		    	System.out.println("Elige el id de la categoría a borrar");
-				int idBorrar = scanner.nextInt();
-				
-				PreparedStatement preparedStatementDelete =connection.prepareStatement("delete from categoria where id = ?");
-	    		
-	    		preparedStatementDelete.setLong(1,idBorrar);
-	    		ResultSet resultSetDelete = preparedStatementDelete.executeQuery();
-	    		
-	    		resultSetDelete.close();
-	
-		        break;
-		    case 2:
-				System.out.println("Introduce el nombre a insertar");
-				String nombreInsertar = scanner.nextLine();
-				
-				PreparedStatement preparedStatementInsert =connection.prepareStatement("insert into categoria (nombre) VALUES (?)");
-	    		
-	    		preparedStatementInsert.setString(1,nombreInsertar);
-	    		ResultSet resultSet = preparedStatementDelete.executeQuery();
-
-		        
-		        break;    
-	        case 3:
+	      
 	        	
 	        	PreparedStatement preparedStatement =connection.prepareStatement("select * from categoria where id < ?");
 	    		
@@ -68,20 +29,9 @@ public class MySql {
 	    		}
 				
 
-		        
-		        break;
-	        case 4:
-	        	
-	        	System.exit(0);         
-		        break;
-	        	
-			}
-			
-			
-			
-			
-			
-		 }while(opcion!=5);
+	        
+	     
+
 
 		
 		resultSet.close();
